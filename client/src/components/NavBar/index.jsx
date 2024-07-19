c29adc7d3d88abec08d06c1e0b95349c298e28aa
axxxay <akkiakshay440@gmail.com>
2024-07-19 12:45:30 +0530

    changes in navbar

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -20,14 +20,6 @@ const NavBar = () => {
         <>
             <nav className="navbar-container">
                 <Link to="/" className="navbar-logo">Keep Notes</Link>
-                <ul className="nav-list">
-                    <li className="nav-item">
-                        <Link to="/" className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/login" className="nav-link">Login</Link>
-                    </li>
-                </ul>
                 <button className="menu-button" onClick={() => setShowMenu(!showMenu)}>
                     {showMenu ? <IoClose className="menu-icon" /> :
                         <LuMenu className="menu-icon" />
@@ -41,14 +33,6 @@ const NavBar = () => {
             {
                 showMenu && (
                     <div className="menu-container">
-                        <ul className="menu-list">
-                            <li className="menu-item">
-                                <Link to="/" className="nav-link">Home</Link>
-                            </li>
-                            <li className="menu-item">
-                                <Link to="/login" className="nav-link">Login</Link>
-                            </li>
-                        </ul>
                         {
                             Cookies.get('jwt_token') && <button className="menu-logout-button" onClick={handleLogout}>Logout</button>
                         }
